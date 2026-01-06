771ddf84a2f897330d1519b1835935bc9cf6edaa
Sanjayng <sanjayng001@gmail.com>
2026-01-06 20:57:25 +0530

    refactor: update favicon and improve layout in Settings page

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -121,8 +121,8 @@ const Settings = () => {
   }
 
   return (
-    <div className="flex-1 flex items-center justify-center bg-linear-to-br from-gray-50 to-gray-100 md:p-4">
-      <div className="w-full md:max-w-2xl bg-white md:rounded-2xl shadow-xl p-2 sm:p-6 md:p-8 overflow-y-auto">
+    <div className="flex-1 h-full flex justify-center bg-linear-to-br from-gray-50 to-gray-100 md:p-4 overflow-y-auto">
+      <div className="w-full h-fit md:max-w-2xl bg-white md:rounded-2xl shadow-xl p-2 sm:p-6 md:p-8">
         {/* Header */}
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold text-gray-800 mb-2">
